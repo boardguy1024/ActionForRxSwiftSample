c47289e03b54775e2db406dedabc928876909774
ptn0406 <parkks@pivot.jp>
2017-11-27 11:41:32 +0900

    pods install

--- ActionForRxSwiftSample/ViewController.swift
+++ ActionForRxSwiftSample/ViewController.swift
@@ -7,19 +7,52 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
+import Action
+
+struct Foo {
+    
+}
+
+class FooViewModel {
+    
+    let reloadTrigger = PublishSubject<Void>()
+    
+}
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
